a79b00937554d1d414a35ce4f9eee2f1539adf6c
micahsgilbert <micahsgilbert@gmail.com>
2023-03-15 20:06:54 -0700

    Add arrow to hero pages

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -5,3 +5,12 @@
     document.addEventListener('touchstart', function(){}, false);
   }
 })();
+
+const scrollToFirstContent = () => {
+  const vh = window.innerHeight
+  if (window.innerWidth < 500) {
+    window.scrollTo(0, vh - 64)
+  } else {
+    window.scrollTo(0, vh - 80)
+  }
+}
