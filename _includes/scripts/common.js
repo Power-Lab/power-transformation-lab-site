b017544946280f495041c0622dad0f50bc73b42f
micahsgilbert <micahsgilbert@gmail.com>
2023-03-15 20:32:07 -0700

    create new hero.hs script

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -5,12 +5,3 @@
     document.addEventListener('touchstart', function(){}, false);
   }
 })();
-
-const scrollToFirstContent = () => {
-  const vh = window.innerHeight
-  if (window.innerWidth < 500) {
-    window.scrollTo(0, vh - 64)
-  } else {
-    window.scrollTo(0, vh - 80)
-  }
-}
