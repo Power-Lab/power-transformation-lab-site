33270849c24961e9d82eecec6ae9c26e6ec10460
micahsgilbert <micahsgilbert@gmail.com>
2023-06-09 11:10:48 -0700

    Add basic filtering funcitonality to publicaitons

--- _includes/scripts/publications.js
+++ _includes/scripts/publications.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("a.box").forEach(box => {
     const filterTitle = box.getAttribute("href")
-    console.log(filterTitle)
     box.addEventListener("click", e => {
       e.preventDefault()
-      console.log(filterTitle)
+      document.querySelectorAll("div.article-container").forEach(div => {
+        if (div.getAttribute("data-encode") == filterTitle) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none"
+        }
+      })
+    })
+  })
+  document.querySelector("#show-all").addEventListener("click", e => {
+    e.preventDefault()
+    document.querySelectorAll("div.article-container").forEach(div => {
+      div.style.display = "block"
     })
   })
 })
